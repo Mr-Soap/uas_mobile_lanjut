ee3bce622eba7362ef4b619e6b6ceb2107d68da6
Mr. Fa <133749021+Mr-Soap@users.noreply.github.com>
2026-06-29 14:03:07 +0700

    inisiasi color theme

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart';
+
+class AppColors {
+  static const primary = Color(0xFF329D9C);
+  static const secondary = Color(0xFF56C596);
+  static const accent = Color(0xFF7BE495);
+  static const background = Color(0xFFF7FBFC);
+  static const darkBlue = Color(0xFF205072);
+  static const textPrimary = Color(0xFF205072);
+  static const textSecondary = Color(0xFF5C6B73);
+}
